3035fd1073416ffb755f0d2c6d2aebf8fc6d6a20
Miguel Guzman <mguzman1@gmail.com>
2024-12-21 09:43:05 -0600

    Agregado CommonModule

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
+  imports: [CommonModule],
   styleUrl: './hero.component.css'
 })
 
