e9e9f8d609b10e272c4679b69f966096e6e65f5a
Miguel Guzman <mguzman1@gmail.com>
2024-12-30 17:51:02 -0600

    Cambio nombre title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Bases Angular';
+  title = 'Bases Angular/Dragon Ball Z';
 }
